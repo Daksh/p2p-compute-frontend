3a0e8990e45e07a109d5edc9634aa5bac2fe9827
Sanskar Jethi <sansyrox@gmail.com>
2022-02-26 16:24:29 +0000

    Docker execution of python file

--- frontend/backend/index.py
+++ frontend/backend/index.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import subprocess
+
 app = Flask(__name__)
 
 
@@ -6,6 +8,15 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/create_container')
+def run_docker_container():
+    # get a python file
+    # store it there
+    # create a docker build
+    file_name = ""
+    # docker-compose run python file_name
+    subprocess.Popen(['docker-compose', 'build', '.'])
+    subprocess.Popen(['docker-compose', 'run', 'python_service', 'python3', file_name])
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000)
