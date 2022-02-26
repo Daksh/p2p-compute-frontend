865e8ab0dc9529b019924138d89c8f9de876ae95
Dragos Vecerdea <dragosvecerdea@Dragoss-MacBook-Pro.local>
2022-02-26 17:02:33 +0000

    landing page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,77 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './Home.scss'
 
 function Home() {
     return (
-      <div className="Home">
-        <header className="Home-header">
-            <Link to="/network">Join network</Link>
-        </header>
-      </div>
+      <body>
+        <div className="container">
+            <div className="container__item landing-page-container">
+                <div className="content__wrapper">
+
+                    {/* <header className="header">
+                        <div className="menu-icon header__item">
+                            <span className="menu-icon__line"></span>
+                        </div>
+
+                        <h1 className="heading header__item">D-UI</h1>
+
+                        <ul className="social-container header__item">
+                            <li className="social__icon social__icon--fb">
+                                <img src="https://image.flaticon.com/icons/svg/1077/1077041.svg" alt="facebook"/>
+                            </li>
+                            <li className="social__icon social__icon--dr">
+                                <img src="https://image.flaticon.com/icons/svg/25/25631.svg" alt="dribbble"/>
+                            </li>
+                            <li className="social__icon social__icon--in">
+                                <img src="https://image.flaticon.com/icons/svg/1077/1077042.svg" alt="instagram"/>
+                            </li>
+                        </ul>
+                    </header> */}
+
+                    <p className="coords">N 49° 16' 35.173"  /  W 0° 42' 11.30"</p>
+
+                    <div className="ellipses-container">
+
+                        <div className="scrolling-words-container">
+                            <div className="scrolling-words-box">
+                                <ul>
+                                <li>P2P</li>
+                                <li>Distributed</li>
+                                <li>Decentrelized</li>
+                                <li>Secured</li>
+                                <li>P2P</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <h6 className="greeting">Arko</h6>
+
+                        <div className="ellipses ellipses__outer--thin">
+
+                            <div className="ellipses ellipses__orbit"></div>
+
+                        </div>
+
+                        <div className="ellipses ellipses__outer--thick"></div>
+                    </div>
+
+                    <div className="scroller">
+                        <p className="page-title">
+                        <Link to="/network">Join the Network</Link>
+                        </p>
+
+                        <div className="timeline">
+                            <span className="timeline__unit"></span>
+                            <span className="timeline__unit timeline__unit--active"></span>
+                            <span className="timeline__unit"></span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+      </body>
     );
   }
 
