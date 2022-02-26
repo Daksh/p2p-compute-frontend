c50e7503785bb02964a536966428be76977cc5f5
Sanskar Jethi <sansyrox@gmail.com>
2022-02-26 18:11:33 +0000

    Sockets working

--- frontend/src/pages/Network.js
+++ frontend/src/pages/Network.js
@@ -1,15 +1,59 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import io from "socket.io-client";
 
-function Network() {
+let endPoint = "http://localhost:5000";
+let socket = io.connect(`${endPoint}`);
+
+class Network extends Component {
+  state = {
+    messages: ["Hello and Welcome"],
+    message: "",
+  };
+
+  componentDidMount = () => {
+    socket.on("message", (msg) => {
+      this.setState({
+        messages: [...this.state.messages, msg],
+      });
+    });
+  };
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  onClick = () => {
+    const { message } = this.state;
+    if (message !== "") {
+      this.setState({
+        message: "",
+      });
+      socket.emit("message", message);
+    } else {
+      alert("Please Add A Message");
+    }
+  };
+
+  render() {
+    const { messages, message } = this.state;
     return (
-      <div className="Home">
-        <header className="Home-header">
-            <div>About the community</div>
-            <Link to="/upload">Upload job</Link>
-        </header>
+      <div>
+        {messages.length > 0 &&
+          messages.map((msg) => (
+            <div>
+              <p>{msg}</p>
+            </div>
+          ))}
+        <input
+          value={message}
+          name="message"
+          onChange={(e) => this.onChange(e)}
+        />
+        <button onClick={() => this.onClick()}>Send Message</button>
       </div>
     );
   }
-
-export default Network
+}
+export default Network;
